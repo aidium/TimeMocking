004d0d39e81203f3af54b4bd37a8eb53cee2c786
aidium <bjorn@aidium.com>
2013-12-19 12:19:44 +0100

    updated the test

--- src/test/java/se/aidium/mock/time/TimingTest.java
+++ src/test/java/se/aidium/mock/time/TimingTest.java
@@ -1,12 +1,23 @@
 package se.aidium.mock.time;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TimingTest {
 
 	@Mock
@@ -16,13 +27,52 @@ public class TimingTest {
 	Timing timing;
 
 	@Test(expected = RuntimeException.class)
-	public void shouldTimoutAndThrow() {
+	public void shouldTimoutAfterFourthCallAndThrowBadImplementation() {
 		// Given
-		given(dataGeneratorMock.generateDatasetWithIndex(anyInt())).willAnwer(
-				new Answer() {
+		given(dataGeneratorMock.generateDatasetWithIndex(anyInt())).will(
+				new Answer<List<Integer>>() {
+
+					@Override
+					public List<Integer> answer(InvocationOnMock invocation)
+							throws Throwable {
+						// Simulate work
+						Thread.sleep(10);
+						return Arrays.asList(1);
+					}
 				});
 
-		Date timeout = new Date(System.currentTimeMillis() + 30000);
-		timing.processLotsOfDataWithTimout(timeout);
+		try {
+			// When
+			Date timeout = new Date(System.currentTimeMillis() + 35);
+			timing.processLotsOfDataWithTimout(timeout);
+		} finally {
+			// Then
+			verify(dataGeneratorMock, times(4)).generateDatasetWithIndex(
+					anyInt());
+		}
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void shouldTimoutAfterFourthCallAndThrow() {
+		// Given
+		given(dataGeneratorMock.generateDatasetWithIndex(anyInt())).will(
+				new Answer<List<Integer>>() {
+
+					@Override
+					public List<Integer> answer(InvocationOnMock invocation)
+							throws Throwable {
+						return Arrays.asList(1);
+					}
+				});
+
+		try {
+			// When
+			Date timeout = new Date(System.currentTimeMillis() + 30);
+			timing.processLotsOfDataWithTimout(timeout);
+		} finally {
+			// Then
+			verify(dataGeneratorMock, times(4)).generateDatasetWithIndex(
+					anyInt());
+		}
 	}
 }
